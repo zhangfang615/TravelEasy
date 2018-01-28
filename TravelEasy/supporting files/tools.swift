76aedb2219acaeb3fd58e1afec6842b8589fdee4
minxin <zhuminxin@zhuminxindeMacBook-Pro.local>
2018-01-27 18:17:57 -0800

    final version1.0

--- TravelEasy/supporting files/tools.swift
+++ TravelEasy/supporting files/tools.swift
@@ -1,33 +1,9 @@
-
 //
 //  tools.swift
 //  TravelEasy
 //
-//  Created by Fang Zhang on 2018/1/25.
+//  Created by zhuminxin on 2018/1/27.
 //  Copyright © 2018年 Fang Zhang. All rights reserved.
 //
 
 import Foundation
-import Toast_Swift
-import SwiftyJSON
-import Alamofire
-
-func makeToast(view: UIView, message : String, duration : Float, point : CGPoint, title : String, image : UIImage?, style : ToastStyle)
-{
-    view.makeToast(message, duration: 1.0, point: point, title: title, image: image, style: style){ didTap in
-        if didTap {
-        } else {
-        }
-    }
-}
-
-func postAPI(param : Dictionary<String, Any>, apiURL : URL, completionHandler: @escaping (JSON?, NSError?) -> ())
-{
-    Alamofire.request(apiURL, method: .post, parameters: param, encoding:  URLEncoding.default, headers: nil).responseJSON { (response) in
-        if(response.error == nil){
-            completionHandler(JSON(response.result.value!), nil)
-        }else{
-            completionHandler(nil, response.error! as NSError)
-        }
-    }
-}
--- TravelEasy/views/InformationViews/SettingsView.swift
+++ TravelEasy/views/InformationViews/SettingsView.swift
@@ -2,36 +2,8 @@
 //  SettingsView.swift
 //  TravelEasy
 //
-//  Created by Fang Zhang on 2018/1/26.
+//  Created by zhuminxin on 2018/1/27.
 //  Copyright © 2018年 Fang Zhang. All rights reserved.
 //
 
 import Foundation
-import UIKit
-
-class SettingsView : UIView {
-    var logoutButton : UIButton!
-    
-    override init(frame : CGRect)
-    {
-        super.init(frame: frame)
-        self.backgroundColor = UIColor.blue
-        createLogoutButton()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func createLogoutButton() {
-        logoutButton = UIButton()
-        
-        logoutButton.setTitle("退出登录", for:.normal)
-        
-        logoutButton.frame = CGRect(x: 20, y: 540, width: 335, height: 40)
-        
-        logoutButton.backgroundColor = UIColor.gray
-        
-        self.addSubview(logoutButton)
-    }
-}
